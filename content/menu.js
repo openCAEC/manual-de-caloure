df09e3a9a8c30c34fe136d0ce910852c5c456d6e
Gustavo Simões <gustavosimoesmendonca@gmail.com>
2022-04-18 03:12:14 -0300

    Modificação no conteúdo do manual
    Signed-off-by: Gustavo Simões <gustavosimoesmendonca@gmail.com>

---
+++ content/menu.js
@@ -0,0 +1,290 @@
+export const menu = [
+  {
+    title: "Prefácio",
+    slug: "/1-prefacio",
+    items: [
+      {
+        title: "Campus Araranguá",
+        id: "campus-ararangua",
+      },
+      {
+        title: "Para para para tudo!",
+        id: "para-para-para-tudo",
+      },
+
+      {
+        title: "Mensagem da coordenação",
+        id: "mensagem-da-coordenacao",
+      },
+
+      {
+        title: "Sobre o gênero dos substantivos neste manual",
+        id: "sobre-o-genero-dos-substantivos-neste-manual",
+      },
+
+      {
+        title: '"Os melhores anos da sua vida"',
+        id: "os-melhores-anos-da-sua-vida",
+      },
+    ],
+  },
+  {
+    title: "Sobrevivendo na UFSC",
+    slug: "/2-sobrevivendo-na-ufsc",
+    items: [
+      {
+        title: "A palavra é permanência",
+        id: "a-palavra-e-permanencia",
+      },
+      {
+        title: "Dois modelos de universidade",
+        id: "dois-modelos-de-universidade",
+      },
+      {
+        title: "Moradia",
+        id: "moradia",
+      },
+      {
+        title: "SAE - Serviço de Apoio ao Estudante",
+        id: "sae-servico-de-apoio-ao-estudante",
+      },
+      {
+        title: "Cuidando da Saúde física",
+        id: "cuidando-da-saude-fisica",
+      },
+      {
+        title: "Cuidando da Saúde Mental",
+        id: "cuidando-da-saude-mental",
+      },
+      {
+        title: "O que comer na UFSC",
+        id: "o-que-comer-na-ufsc",
+      },
+    ],
+  },
+  {
+    title: "Estudando na UFSC",
+    slug: "/3-estudando-na-ufsc",
+    items: [
+      {
+        title: "Siglas/ Dicionário",
+        id: "siglas-dicionario",
+      },
+
+      {
+        title: "Horário de funcionamento do Campus",
+        id: "horario-de-funcionamento-do-campus",
+      },
+
+      {
+        title: "Horário de Aulas",
+        id: "horario-de-aulas",
+      },
+      {
+        title: "Currículo do curso",
+        id: "curriculo-do-curso",
+      },
+
+      {
+        title: "Datas Importantes – Calendário UFSC",
+        id: "datas-importantes-calendario-ufsc",
+      },
+
+      {
+        title: "Disciplinas",
+        id: "disciplinas",
+      },
+
+      {
+        title: "Códigos de curso",
+        id: "codigos-de-curso",
+      },
+
+      {
+        title: "Alguns sistemas da graduação",
+        id: "alguns-sistemas-da-graduacao",
+      },
+
+      {
+        title: "IA e Reprovação",
+        id: "ia-e-reprovacao",
+      },
+
+      {
+        title: "“Pra quê estudar isso?”",
+        id: "pra-que-estudar-isso",
+      },
+
+      {
+        title: "Bem-vinde à academia!",
+        id: "bem-vinde-a-academia",
+      },
+
+      {
+        title: "Cursos extracurriculares de idiomas (cursos online)",
+        id: "cursos-extracurriculares-de-idiomas-cursos-online",
+      },
+
+      {
+        title: "Carteirinha de estudante",
+        id: "carteirinha-de-estudante",
+      },
+
+      {
+        title: "Biblioteca Universitária - BU",
+        id: "biblioteca-universitaria-bu",
+      },
+
+      {
+        title: "Sala de estudos",
+        id: "sala-de-estudos",
+      },
+
+      {
+        title: "Benefícios",
+        id: "beneficios",
+      },
+
+      {
+        title: "Trancamento e desistência",
+        id: "trancamento-e-desistencia",
+      },
+    ],
+  },
+  {
+    title: "Convivendo na UFSC — e no mundo!",
+    slug: "/4-convivendo-na-ufsc",
+    items: [
+      {
+        title: "Pessoas e vivências diferentes",
+        id: "pessoas-e-vivencias-diferentes",
+      },
+
+      {
+        title: "Mulheres na computação",
+        id: "mulheres-na-computacao",
+      },
+
+      {
+        title: "O que conhecemos como mulher",
+        id: "o-que-conhecemos-como-mulher",
+      },
+
+      {
+        title: "A universidade reflete a sociedade",
+        id: "a-universidade-reflete-a-sociedade",
+      },
+
+      {
+        title: "Política de segurança pública",
+        id: "politica-de-seguranca-publica",
+      },
+
+      {
+        title: "Guia para convivência com as mulheres",
+        id: "guia-para-convivencia-com-as-mulheres",
+      },
+
+      {
+        title: "Negros e negras na computação",
+        id: "negros-e-negras-na-computacao",
+      },
+
+      {
+        title: "Muito o que ser feito",
+        id: "muito-o-que-ser-feito",
+      },
+
+      {
+        title: "Um projeto político",
+        id: "um-projeto-politico",
+      },
+
+      {
+        title: "Luta é o início de um reconhecimento",
+        id: "luta-e-o-inicio-de-um-reconhecimento",
+      },
+
+      {
+        title: "Gênero e sexualidade",
+        id: "genero-e-sexualidade",
+      },
+
+      {
+        title: "Falando de identidade de gênero",
+        id: "falando-de-identidade-de-genero",
+      },
+
+      {
+        title: "O que ocorre na prática",
+        id: "o-que-ocorre-na-pratica",
+      },
+
+      {
+        title: "Virando o jogo",
+        id: "virando-o-jogo",
+      },
+
+      {
+        title: "Nota final S2",
+        id: "nota-final-s2",
+      },
+    ],
+  },
+  {
+    title: "Vivendo em Araranguá",
+    slug: "/5-vivendo-em-aru",
+    items: [
+      {
+        title: "Aluguel",
+        id: "aluguel",
+      },
+
+      {
+        title: "Cuidado com os Golpes",
+        id: "cuidado-com-os-golpes",
+      },
+
+      {
+        title: "Alimentação e Serviços",
+        id: "alimentacao-e-servicos",
+      },
+
+      {
+        title: "Diversão e Transporte",
+        id: "diversao-e-transporte",
+      },
+    ],
+  },
+
+  {
+    title: "Além da graduação",
+    slug: "/6-alem-da-graduacao",
+    items: [
+      {
+        title: "CAEC, o Centro Acadêmico de Engenharia da Computação",
+        id: "caec-o-centro-academico-de-engenharia-da-computacao",
+      },
+
+      {
+        title: "SAEC",
+        id: "saec",
+      },
+
+      {
+        title: "Laboratórios",
+        id: "laboratorios",
+      },
+
+      {
+        title: "AAACA",
+        id: "aaaca",
+      },
+
+      {
+        title: "Cursinho PES",
+        id: "cursinho-pes",
+      },
+    ],
+  },
+];
